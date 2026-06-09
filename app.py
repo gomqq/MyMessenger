28410482f04cdd0bd7f69e7b42a9403b3bb5f44c
sargeludima-prog <sargeludima@gmail.com>
2026-06-09 17:20:39 +0200

    Fix Render folders

--- app.py
+++ app.py
@@ -13,6 +13,9 @@ app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 VOICE_FOLDER = "static/voices"
 app.config["VOICE_FOLDER"] = VOICE_FOLDER
 
+os.makedirs("static/images", exist_ok=True)
+os.makedirs("static/voices", exist_ok=True)
+
 IMAGE_FOLDER = "static/images"
 app.config["IMAGE_FOLDER"] = IMAGE_FOLDER
 
